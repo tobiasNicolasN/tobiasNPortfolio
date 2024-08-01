0fd467b139b1571cc62a504c85f1ed274d58ccb9
Tobias Nuñez <tobias.nicolas001@gmail.com>
2024-08-01 14:49:46 -0300

    Portfolio page adding info

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -11,10 +11,11 @@ export default function Portfolio() {
 
   return (
     <div>
-      <div className="w-full flex flex-col items-center mt-20 md:mt-36 lg:mt-40">
+      <div className="w-full flex flex-col items-center mt-20 md:mt-36 lg:mt-40 mb-12">
         <NavBar />
         <main className="flex w-11/12 md:w-10/12 lg:w-10/12 xl:w-9/12 2xl:w-7/12 flex-col justify-center font-sans">
           <div className="animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in"></div>
+
           <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl mt-2 font-semibold font-mono text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
             Portfolio
           </h1>
@@ -28,20 +29,38 @@ export default function Portfolio() {
               </div>
             ))}
           </div>
-          <h1 className="text-sm md:text-base lg:text-lg xl:text-xl tracking-wider mt-4 text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in ">
+
+
+          <h1 className="mt-8 text-lg md:text-xl lg:text-xl xl:text-xl font-semibold font-mono text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
+            {
+              lang ? "Formulario de Envío de Correos" : "Email Submission Form"
+            }
+          </h1>
+          <h1 className="text-sm md:text-base lg:text-lg xl:text-xl tracking-wider text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
             {lang
               ? 'Los correos electrónicos enviados desde la página son creados utilizando Node.js y Resend. Esta funcionalidad está implementada a través de un endpoint de la API dentro del framework Next.js.'
-              : 'Emails sent from the page are created using Node.js and Resend. This functionality is implemented through an API endpoint within the Next.js framework .'}
+              : 'Emails sent from the page are created using Node.js and Resend. This functionality is implemented through an API endpoint within the Next.js framework.'}
           </h1>
-          <h1 className="text-sm md:text-base lg:text-lg xl:text-xl tracking-tracking-wider mt-2 text-gray-600 dark:text-gray-300 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
+          <h1 className="text-sm md:text-base lg:text-lg xl:text-xl mt-2 tracking-wider text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
+            {lang
+              ? 'El manejo de errores para el envío de correos electrónicos se realiza con react-hook-form, ofreciendo una forma robusta de validar formularios y manejar errores. Esto asegura que los usuarios reciban retroalimentación clara si surgen problemas durante el envío de correos.'
+              : 'Error handling for email sending is managed with react-hook-form, providing a robust way to validate forms and handle errors. This ensures users receive clear feedback if issues arise during email submission.'}
+          </h1>
+
+          <h1 className="text-lg md:text-xl mt-8 lg:text-xl xl:text-xl font-semibold font-mono text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
+            {
+              lang ? "Hosting y Configuración de Dominio" : "Hosting and Domain Configuration"
+            }
+          </h1>
+          <h1 className="text-sm md:text-base lg:text-lg xl:text-xl tracking-wider text-gray-800 dark:text-gray-200 animate-fade-right animate-once animate-duration-[400ms] animate-delay-100 animate-ease-in">
             {lang ? (
               <>
                 Desplegado utilizando{' '}
-                <a target="_blank" href="https://railway.app">
+                <a target="_blank" href="https://railway.app" className='text-second-light dark:text-second'>
                   Railway
                 </a>
                 , con un dominio personalizado gestionado por{' '}
-                <a target="_blank" href="https://www.hostinger.com.ar/">
+                <a target="_blank" href="https://www.hostinger.com.ar/" className='text-second-light dark:text-second'>
                   Hostinger
                 </a>
                 .
@@ -49,11 +68,11 @@ export default function Portfolio() {
             ) : (
               <>
                 Deployed using{' '}
-                <a target="_blank" href="https://railway.app" className='text-second-light dark:text-second-light'>
+                <a target="_blank" href="https://railway.app" className='text-second-light dark:text-second'>
                   Railway
                 </a>
                 , featuring a custom domain managed by{' '}
-                <a target="_blank" href="https://www.hostinger.com.ar/" className='text-second-light dark:text-second-light'>
+                <a target="_blank" href="https://www.hostinger.com.ar/" className='text-second-light dark:text-second'>
                   Hostinger
                 </a>
                 .
